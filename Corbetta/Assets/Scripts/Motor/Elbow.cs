8ae524a460e6fc454e9326fb40b9e774177c86f4
Paulina Friemann <paulina@friemann.com>
2016-05-08 18:56:03 +0200

    basic unity project with body components etc

---
+++ Corbetta/Assets/Scripts/Motor/Elbow.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Elbow : MonoBehaviour {
+
+    public GameObject elbowJoint;
+
+    float jointAngle = 0;
+    float targetRotation = 0;
+
+
+    public float GetAngle()
+    {
+        return jointAngle;
+    }
+
+    public void SetAngle(float angle)
+    {
+        targetRotation = angle;
+    }
+}
